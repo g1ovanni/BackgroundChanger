d61016099d9a9535c1a8e1644602417418fb587b
Athena <giovanni.gab@icloud.com>
2022-01-16 21:18:05 +0100

    Index.html file

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// ! ///////////////////////////////////////
+// ! Breakdown
+
+/* 
+
+0. Create event lister for Create button			✅
+1. Save number of characters for the variable		✅
+1.1. When there is no input							✅
+2. Generate random characters for each position		✅
+3. Show password									✅
+4. Click again to enter a new number				✅
+
+*/
+
+// const passwordLength = 0;
+// 0. Create event lister for 'Create' button
+// 1. Save number of characters for the variable
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.create').addEventListener('click', function () {
+  const passwordLength = Number(
+    document.querySelector('.passwordLength').value
+  );
+  const characters =
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!$%&/()=?+^*,.;:-_';
+  const charactersLength = characters.length;
+  // Array definition ('password')
+  const password = new Array(passwordLength);
+  let passFinal = 0;
+  let randomNum = 0;
+
+  // 2. Generate random characters for each position
+  // Creating a random password
+  for (let i = 0; i < passwordLength; i++) {
+    randomNum = Math.floor(Math.random() * charactersLength);
+    password[i] = characters.charAt(randomNum);
+  }
+  passFinal = password.join('');
+
+  // 1.1. When there is no input
+  if (!passwordLength || passwordLength > 20 || passwordLength < 8) {
+    displayMessage('The password should have between 8 and 20 characters');
+    document.getElementById('password').style.width = '15rem';
+    document.querySelector('.password').textContent = '?';
+    document.getElementById('password').style.fontFamily = 'Press Start 2P';
+  }
+  //   Showing the new password
+  else {
+    document.querySelector('.password').textContent = passFinal;
+    document.getElementById('password').style.width = '1500px';
+    document.getElementById('password').style.fontFamily = 'VT323';
+    document.querySelector('.message').textContent =
+      'There is your new password!';
+  }
+});
